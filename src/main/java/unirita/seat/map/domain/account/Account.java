1bd066451215fe0b30913dafaf8dcee557a230a5
BSP\97944 <rikiya_ninomiya@unirita.co.jp>
2018-01-24 09:52:40 +0900

    Slack通知の確認

---
+++ src/main/java/unirita/seat/map/domain/account/Account.java
@@ -0,0 +1,47 @@
+package unirita.seat.map.domain.account;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "accounts")
+public class Account {
+	private String id;
+	private String name;
+	private String password;
+	private boolean admin;
+
+	public String getId(){
+		return id;
+	}
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getPassword(){
+		return password;
+	}
+
+	public void setPassword(String password){
+		this.password = password;
+	}
+
+	public boolean getAdmin(){
+		return admin;
+	}
+
+	public void setAdmin(boolean admin){
+		this.admin = admin;
+	}
+
+}
