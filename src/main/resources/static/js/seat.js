46868dcc750b417f37c83a15cf82bcd395c2ed6b
97965 <ryota_shinzato@unirita.co.jp>
2018-03-06 10:45:11 +0900

    座席情報を保持する

--- src/main/resources/static/js/seat.js
+++ src/main/resources/static/js/seat.js
@@ -1,21 +1,53 @@
 var map = L.map('map', { crs: L.CRS.Simple });
 
-var bounds = [[0, 0], [480, 640]]; // ここでは画像の解像度をboundsに設定する。
-L.imageOverlay('seat.png', bounds).addTo(map); // 背景画像を設定する。
+var bounds = [[0, 0], [480, 640]];
+L.imageOverlay('seat.png', bounds).addTo(map);
+let seat = [];
+let count = 0;
 
-function onButtonClick() {
+window.onload = function () {
+    console.log("window.onload");
+}
+
+function onCreateButtonClick() {
     console.log("Button Clicked");
-    var column = document.getElementById("column").value;
-    var line = document.getElementById("line").value;
-    var start_line = document.getElementById("start_line").value;
-    var start_column = document.getElementById("start_column").value;
+    const columnValue = document.getElementById("column").value;
+    const lineValue = document.getElementById("line").value;
+    const start_lineValue = document.getElementById("start_line").value;
+    const start_columnValue = document.getElementById("start_column").value;
+    if (seat.length == 0) {
+        seat = [
+            {
+                column: columnValue,
+                line: lineValue,
+                start_column: start_columnValue,
+                start_line: start_lineValue
+            }
+        ];
+    } else {
+        seat.push(
+            {
+                column: columnValue,
+                line: lineValue,
+                start_column: start_columnValue,
+                start_line: start_lineValue
+            }
+        );
+    }
+    console.log(seat);
 
-    for (var i = line; i > 0; i--) {
-        for (var j = 0; j < column; j++) {
-            L.circle([770 - ((start_line - 1) * 70) - i * 70, start_column * 70 + j * 70], { color: 'green', radius: 35 }).addTo(map)
-                .bindTooltip("hoge", { permanent: true, direction: 'center' }).openTooltip()
-                .bindPopup("hoge");
+    for (let i = 0; i < lineValue; i++) {
+        for (let j = 0; j < columnValue; j++) {
+            L.circle([770 - ((start_lineValue - 1) * 70) - i * 70, start_columnValue * 70 + j * 70], { color: 'green', radius: 35 }).addTo(map)
+                .bindTooltip(String(count), { permanent: true, direction: 'center' }).openTooltip()
+                .bindPopup(String(count));
+            count++;
         }
     }
     map.fitBounds(bounds);
+    return false;
+}
+
+function onSaveButtonClick() {
+    //保存時の処理を書く
 }
